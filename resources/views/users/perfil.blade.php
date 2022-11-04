a7a3473b132248fb39b659672c9ed8e41fb440ae
Youk App <117310103+appyouk@users.noreply.github.com>
2022-11-04 17:57:50 -0300

    Teste 1

--- resources/views/users/perfil.blade.php
+++ resources/views/users/perfil.blade.php
@@ -10,7 +10,7 @@
 			<div class="col-sm-6 p-md-0">
 				<div class="welcome-text">
 					<h4>Olá, {{ Auth::user()->name }}!</h4>
-					<p class="mb-0">Gerencie suas informações, privacidade e segurança...</p>
+					<p class="mb-0">Gerencie suas informações, privacidade e segurança. Gabriel Teste</p>
 				</div>
 			</div>
 			<div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
